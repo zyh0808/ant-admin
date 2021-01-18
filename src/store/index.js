7864ed6af033c9ffed2668a76596a1252eada8ef
zyh0808 <846408452@qq.com>
2021-01-18 20:02:42 +0800

    add store/user

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import createLogger from './logger/logger'
 import menu from './modules/menu'
+import user from './modules/user'
 
 Vue.use(Vuex)
 const debug = process.env.NODE_ENV !== 'production'
 
 export default new Vuex.Store({
   modules: {
-    menu
+    menu,
+    user
   },
   strict: debug,
   plugins: debug ? [createLogger()] : []
